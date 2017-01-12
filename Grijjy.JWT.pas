0e82e73f3f96a8ada0e8e466b04de911f7ab6dd1
Allen Drennan <allendrennan@gmail.com>
2017-01-12 10:40:12 -0800

    New unit for Java Web Tokens

---
+++ Grijjy.JWT.pas
@@ -0,0 +1,40 @@
+unit Grijjy.JWT;
+
+{ Java Web Tokens }
+
+interface
+
+uses
+  System.SysUtils;
+
+const
+  { RSA with SHA256 }
+  JWT_RS256 = '{"alg":"RS256","typ":"JWT"}';
+
+  { TODO: Added HMAC with SHA256 token support }
+
+{ Creates a Java Web Token using the provided private key in PEM format }
+function JavaWebToken(const APrivateKey: TBytes; const AHeader, APayload: String; out AJWT: String): Boolean;
+
+implementation
+
+uses
+  Grijjy.OpenSSL.API,
+  Grijjy.BinaryCoding;
+
+function JavaWebToken(const APrivateKey: TBytes; const AHeader, APayload: String; out AJWT: String): Boolean;
+var
+  Data: TBytes;
+  JWS: TBytes;
+begin
+  Data := goBase64Encode(BytesOf(AHeader)) + [Ord('.')] + goBase64Encode(BytesOf(APayload));
+  if TgoSSLHelper.Sign_RSASHA256(Data, APrivateKey, JWS) then
+  begin
+    AJWT := StringOf(Data) + '.' + StringOf(goBase64Encode(JWS));
+    Result := True;
+  end
+  else
+    Result := False;
+end;
+
+end.
