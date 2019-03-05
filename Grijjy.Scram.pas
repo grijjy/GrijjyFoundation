6a1860e8238de984162989b259f5a5ef1a3bf1ba
Allen Drennan <allendrennan@gmail.com>
2019-03-05 12:57:02 -0800

    SCRAM support, SHA-1 and SHA-256

--- Grijjy.Scram.pas
+++ Grijjy.Scram.pas
@@ -141,16 +141,16 @@ end;
 
 function SaslPrepPassword(APassword: String): String;
 var
-	I: Integer;
+  I: Integer;
 begin
-	Result := APassword;
-	for I := 1 to Length(APassword) do
-		case Word(APassword[I]) of
-		  $00A0, $1680, $2000, $2001, $2002, $2003, $2004, $2005, $2006, $2007, $2008, $2009, $200A, $202F, $205F, $3000:
-			begin
-				Result[I] := #$0020;
-			end;
-		end;
+  Result := APassword;
+  for I := 1 to Length(APassword) do
+    case Word(APassword[I]) of
+      $00A0, $1680, $2000, $2001, $2002, $2003, $2004, $2005, $2006, $2007, $2008, $2009, $200A, $202F, $205F, $3000:
+      begin
+        Result[I] := #$0020;
+      end;
+    end;
 end;
 
 { TgoScram }
